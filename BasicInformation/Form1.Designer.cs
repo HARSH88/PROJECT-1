2c08a59b228800a72ebd331fdb55cdd6d985a81c
HARSH SHAH <harsh_shah88@hotmail.com>
2013-01-28 15:02:54 -0500

    Submission.

---
+++ BasicInformation/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace BasicInformation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btndisplay = new System.Windows.Forms.Button();
+            this.rtxtshow = new System.Windows.Forms.RichTextBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btndisplay
+            // 
+            this.btndisplay.Location = new System.Drawing.Point(83, 12);
+            this.btndisplay.Name = "btndisplay";
+            this.btndisplay.Size = new System.Drawing.Size(75, 23);
+            this.btndisplay.TabIndex = 0;
+            this.btndisplay.Text = "DISPLAY";
+            this.btndisplay.UseVisualStyleBackColor = true;
+            this.btndisplay.Click += new System.EventHandler(this.btndisplay_Click);
+            // 
+            // rtxtshow
+            // 
+            this.rtxtshow.Location = new System.Drawing.Point(12, 42);
+            this.rtxtshow.Name = "rtxtshow";
+            this.rtxtshow.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.rtxtshow.Size = new System.Drawing.Size(287, 145);
+            this.rtxtshow.TabIndex = 1;
+            this.rtxtshow.Text = "";
+            this.rtxtshow.TextChanged += new System.EventHandler(this.rtxtshow_TextChanged);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(83, 193);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 2;
+            this.btnsave.Text = "SAVE";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 268);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.rtxtshow);
+            this.Controls.Add(this.btndisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btndisplay;
+        private System.Windows.Forms.RichTextBox rtxtshow;
+        private System.Windows.Forms.Button btnsave;
+    }
+}
+
---
+++ BasicInformation/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//ADDED:    for xml
+using System.Xml;
+
+namespace BasicInformation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// This will read valid XML file and then display in richtext box.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btndisplay_Click(object sender, EventArgs e)
+        {
+            XmlDocument myDocument = new XmlDocument();
+            try
+            {
+                myDocument.Load("BasicInfo.xml");   //loading the file
+            }
+            catch (System.IO.FileNotFoundException ex)
+            {
+                MessageBox.Show("Can't find file." + Environment.NewLine + ex.Message.ToString());
+            }
+            catch (SystemException ex)
+            {
+                MessageBox.Show("Something is wrong: " + ex.Message.ToString());
+            }
+
+            
+            //READING DATA FROM DOCUMENT and also can be done for multiple nodes and child nodes.
+            for (int curIndex = 0; curIndex != myDocument.DocumentElement.ChildNodes.Count; curIndex++)
+            {
+                XmlNode curNode = myDocument.DocumentElement.ChildNodes[curIndex];
+
+                if (curNode.Name == "name")
+                {
+                   // this.AddLine("NAME# " + curNode.InnerText.ToString());    //can also add any text particular if you want
+                    this.AddLine(curNode.InnerText.ToString());
+                }
+            
+            }
+        }
+        
+            
+            /// <summary>
+            /// this is for the placement of line in text box.
+            /// </summary>
+            /// <param name="theText"></param>
+            private void AddLine(string theText)
+            {
+                //this.rtxtshow.Text = this.rtxtshow.Text + Environment.NewLine + theText.ToString();   //if add in new line
+                this.rtxtshow.Text = this.rtxtshow.Text + theText.ToString();
+            }
+
+            /// <summary>
+            /// it saves the data edited in the same file and gives a message box that its done.. and also can be done for multiple nodes
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            private void btnsave_Click(object sender, EventArgs e)
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml("BasicInfo.xml");                 //taking new file and saving in it....
+                ds.Tables[0].Rows[0]["name"] = rtxtshow.Text;   //taking node and saving it
+                ds.AcceptChanges();
+                ds.WriteXml("BasicInfo.xml");
+
+                MessageBox.Show("FILE UPDATED....");
+            }
+
+            private void rtxtshow_TextChanged(object sender, EventArgs e)
+            {
+
+            }
+
+           
+
+
+        }
+    }
+
